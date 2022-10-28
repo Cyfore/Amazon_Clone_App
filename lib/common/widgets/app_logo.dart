0bcde7bba4db6117699e10e5449f4f55b38d8306
Emirhan Ülker <emr.brk.265@gmail.com>
2022-10-28 03:52:51 +0300

    app-logo

---
+++ lib/common/widgets/app_logo.dart
@@ -0,0 +1,12 @@
+import 'package:amazon_clone_app/constants/consts.dart';
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      ImageConstants.amazon,
+    );
+  }
+}
