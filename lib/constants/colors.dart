5640bc081cb20a30d6b3bdbd685ccee313dcc65a
Emirhan Ülker <emr.brk.265@gmail.com>
2022-11-23 20:11:26 +0300

    constants

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -15,4 +15,12 @@ class MyColors {
     bostonBlue,
     brightTurquoise,
   ]);
+
+    static const appBarGradient2 = LinearGradient(
+    colors: [
+      Color.fromARGB(255, 29, 201, 192),
+      Color.fromARGB(255, 125, 221, 216),
+    ],
+    stops: [0.5, 1.0],
+  );
 }
--- lib/constants/consts.dart
+++ lib/constants/consts.dart
@@ -1,11 +1,10 @@
 export './colors.dart';
 export './icons.dart';
 export './images.dart';
-export './styles.dart';
 export 'app_constants.dart';
 export 'package:velocity_x/velocity_x.dart';
 export 'package:flutter/material.dart';
-export 'helper_methods.dart';
+export '../common/widgets/helper_methods.dart';
 export 'package:get/get.dart';
 export './paddings.dart';
 export 'package:nil/nil.dart';
